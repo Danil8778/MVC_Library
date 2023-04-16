b54b058663181858786210ef67e8ea1ad37dee28
Danil Pnevsky <dpnevsky@gmail.com>
2023-04-16 16:51:11 +0300

    add view "index"

--- src/main/java/pnevsky/com/DAO/PersonDAO.java
+++ src/main/java/pnevsky/com/DAO/PersonDAO.java
@@ -20,7 +20,7 @@ public class PersonDAO {
     }
 
     public List<Person> index() {
-        return jdbcTemplate.query("SELECT * FROM Person",
+        return jdbcTemplate.query("SELECT * FROM People",
                 new BeanPropertyRowMapper<>(Person.class));
     }
 

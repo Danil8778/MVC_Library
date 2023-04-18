366f5595d673e5082854a73d270242173753c53d
Danil Pnevsky <dpnevsky@gmail.com>
2023-04-18 09:44:28 +0300

    add Book

---
+++ src/main/java/pnevsky/com/models/Book.java
@@ -0,0 +1,55 @@
+package pnevsky.com.models;
+
+
+public class Book {
+    private int id;
+
+    private String title;
+
+    private String author;
+
+    private int year;
+
+    public Book() {
+
+    }
+
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+}
